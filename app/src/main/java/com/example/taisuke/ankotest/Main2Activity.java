a8f1209baa1c1e88a49bdbf11217eea11fa233a4
taisuke <f.taisuke0812@gmail.com>
2018-09-15 22:00:21 +0900

    update

--- app/src/main/java/com/example/taisuke/ankotest/Main2Activity.java
+++ app/src/main/java/com/example/taisuke/ankotest/Main2Activity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import javax.crypto.NullCipher;
 
 public class Main2Activity extends AppCompatActivity {
     //private Intent intent = new Intent(getApplication(), QrCodeReadInViewActivity.class);
@@ -12,11 +15,18 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent i = getIntent();
+        if(i.getStringExtra("DATA") != null) {
+            String name = i.getStringExtra("DATA");
+            TextView textView = findViewById(R.id.ReadData);
+            textView.setText(name);
+        }
+
         findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), QrCodeReadInViewActivity.class);
-                startActivity(intent);
+                Intent intent1 = new Intent(getApplication(), QrCodeReadInViewActivity.class);
+                startActivity(intent1);
             }
         });
         findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
@@ -24,6 +34,13 @@ public class Main2Activity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent2 = new Intent(getApplication(), Kuchikomi.class);
                 startActivity(intent2);
+            }
+        });
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(getApplication(), Graph.class);
+                startActivity(intent3);
             }
         });
     }
